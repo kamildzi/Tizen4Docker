d2cbe6dcc116f1f48b24e6229e87e087dcb7cd09
kamildzi <kamildzi@kozacki.pl>
2020-05-22 02:07:15 +0200

    Gnome Keyring support - for Certificate Manager

--- tizen-ide/files/opt/scripts/runTizenIDE.sh
+++ tizen-ide/files/opt/scripts/runTizenIDE.sh
@@ -2,5 +2,18 @@
 # @K.Dziuba
 # Tizen IDE install and start script wrapper (installation should be invoked only when it's needed)
 
-/opt/scripts/installTizenIDE.sh &&
-~/tizen-studio/ide/TizenStudio.sh
+# gnome-keyring support: link keyring data
+VOLUME_DATA_DIR=~/tizen-studio-data/_EXT_TIZEN4DOCKER_/.local
+if [[ ! -d $VOLUME_DATA_DIR ]]; then
+    mkdir --parents $VOLUME_DATA_DIR
+fi
+
+rm -rf ~/.local
+ln -s $VOLUME_DATA_DIR ~/.local
+
+# init dbus, run the IDE
+dbus-run-session -- bash -c '\
+    echo 12345 | gnome-keyring-daemon --unlock \
+    \
+    /opt/scripts/installTizenIDE.sh && \
+    ~/tizen-studio/ide/TizenStudio.sh'
