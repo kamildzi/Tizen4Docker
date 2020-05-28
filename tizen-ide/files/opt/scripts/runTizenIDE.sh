ecad209d8d2390bb6bf02564f8db4502f2798225
Kamil D <kamildzi@users.noreply.github.com>
2020-05-28 20:40:13 +0200

    Bugfix ide fresh install (#13)
    * #12: fresh install bugfix
    
    * #12: strict mode at runTizenIDE.sh enabled

--- tizen-ide/files/opt/scripts/runTizenIDE.sh
+++ tizen-ide/files/opt/scripts/runTizenIDE.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # @K.Dziuba
 # Tizen IDE install and start script wrapper (installation should be invoked only when it's needed)
+set -eu
 
 # gnome-keyring support: link keyring data
 VOLUME_DATA_DIR=~/tizen-studio-data/_EXT_TIZEN4DOCKER_/.local
@@ -13,7 +14,7 @@ ln -s $VOLUME_DATA_DIR ~/.local
 
 # init dbus, run the IDE
 dbus-run-session -- bash -c '\
-    echo 12345 | gnome-keyring-daemon --unlock \
+    echo 12345 | gnome-keyring-daemon --unlock && \
     \
     /opt/scripts/installTizenIDE.sh && \
     ~/tizen-studio/ide/TizenStudio.sh'
